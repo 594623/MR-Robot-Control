00fcd3d7361eccc381f1400cb5ec8885c701ff67
krist <594623@stud.hvl.no>
2023-04-28 22:51:28 +0200

    Got ur_rtde working

--- src/joint_publisher.py
+++ src/joint_publisher.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
 import rospy
-from ur_rtde.rtde import RTDEIOInterface, RTDEControlInterface
+from rtde_control import RTDEControlInterface
+from rtde_receive import RTDEReceiveInterface
+from rtde_io import RTDEIOInterface
 
 class JointPublisher:
     def __init__(self):
         rospy.init_node("joint_publisher", anonymous=False)
+
+        # Connects to the robot
+        self.rtde_c = RTDEControlInterface("0.0.0.0")
+        self.rtde_r = RTDEReceiveInterface("0.0.0.0")
 
     def main(self):
         rospy.spin()
