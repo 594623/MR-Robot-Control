6c2bf99eaa14eec7183101e097212892c7a7b004
krist <594623@stud.hvl.no>
2023-05-01 14:47:41 +0200

    Set up creation of the JointState message

--- src/joint_publisher.py
+++ src/joint_publisher.py
@@ -10,19 +10,30 @@ class JointPublisher:
     def __init__(self):
         rospy.init_node("joint_publisher", anonymous=False)
 
+        # Creates a publisher for the "/joints" ROS topic
+        self.joint_pub = rospy.Publisher("/joints", JointState, queue_size=10)
+
         # Connects to the robot
         self.rtde_c = RTDEControlInterface("0.0.0.0")
         self.rtde_r = RTDEReceiveInterface("0.0.0.0")
-        # Creates a publisher for the "/joints" ROS topic
-        self.joint_pub = rospy.Publisher("/joints", JointState, queue_size=10)
 
     def main(self):
         r = rospy.Rate(1)
         while not rospy.is_shutdown():
             # Gets actual joint data
-            joints = self.rtde_r.getActualQ()
+            joint_positions = self.rtde_r.getActualQ()
+            joint_velocities = self.rtde_r.getActualQd()
+
+            # Creates the JointState message
+            joint_msg = JointState()
+            joint_msg.header.stamp = rospy.Time.now()
+            joint_msg.name = ['shoulder_link', 'upper_arm_link', 'forearm_link', 'wrist_1_link', 'wrist_2_link', 'wrist_3_link']
+            joint_msg.position = joint_positions
+            joint_msg.velocity = joint_velocities
+
             # Publishes the data to the ROS topic
             self.joint_pub.publish(joints)
+            
             r.sleep()
 
 if __name__ == '__main__':
