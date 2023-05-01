0daf4ccbdbc3d7679612e83fef8bb876c69a0251
krist <594623@stud.hvl.no>
2023-05-01 14:11:19 +0200

    Set up the joint publisher

--- src/joint_publisher.py
+++ src/joint_publisher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+from sensor_msgs.msg import JointState
 from rtde_control import RTDEControlInterface
 from rtde_receive import RTDEReceiveInterface
 from rtde_io import RTDEIOInterface
@@ -12,9 +13,17 @@ class JointPublisher:
         # Connects to the robot
         self.rtde_c = RTDEControlInterface("0.0.0.0")
         self.rtde_r = RTDEReceiveInterface("0.0.0.0")
+        # Creates a publisher for the "/joints" ROS topic
+        self.joint_pub = rospy.Publisher("/joints", JointState, queue_size=10)
 
     def main(self):
-        rospy.spin()
+        r = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            # Gets actual joint data
+            joints = self.rtde_r.getActualQ()
+            # Publishes the data to the ROS topic
+            self.joint_pub.publish(joints)
+            r.sleep()
 
 if __name__ == '__main__':
     joint_publisher = JointPublisher()
